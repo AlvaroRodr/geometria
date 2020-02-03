6cb2171201b6726324b5f27d7aab2b4c3319b53a
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2020-02-03 11:15:17 +0100

    versión comentada

--- geometria/src/Rectangulo_ARM.java
+++ geometria/src/Rectangulo_ARM.java
@@ -1,19 +1,32 @@
-
+/**
+ * 
+ * @author Alvaro Rodriguez
+ * @version 1.2
+ */
 public class Rectangulo_ARM extends FiguraGeometrica_ARM {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * 
+	 * @param tipoFigura Parametro que recibe un String
+	 * @param lG Parametro que indica el tamaño de los lados grandes
+	 * @param lP Parametro que indica el tamaño de los lados pequeños
+	 */
 	public Rectangulo_ARM(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	/**
+	 * @return Devuelve el resultado de la operación
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
-	
+	/**
+	 * @return Devuelve el resultado de la operación
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
