9a44fbc69af1f27b28c70fc07d2a329740f78aa3
Álvaro Rodríguez <alvaro.rodriguez.martinez98@gmail.com>
2020-02-03 11:19:18 +0100

    siempre positivo

--- geometria/src/Circulo_ARM.java
+++ geometria/src/Circulo_ARM.java
@@ -6,6 +6,9 @@ public class Circulo_ARM extends FiguraGeometrica_ARM {
 	
 	public Circulo_ARM(double r, String tipoFigura) {
 		super(tipoFigura);
+		if (r < 0) {
+			r=r*(-1);
+		}
 		radio = r;
 	}
 
